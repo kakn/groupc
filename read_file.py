4124eb815763b18f52017d4cd047fc31d97351f2
kaki <kaki@itu.dk>
2020-02-21 01:09:14 +0100

    The code now removes stopwords + some extra character apostrophe combination like 'n.

--- read_file.py
+++ read_file.py
@@ -4,7 +4,11 @@ import csv
 import re
 import langid
 from langid.langid import LanguageIdentifier, model
-
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import PorterStemmer 
+from nltk.stem import LancasterStemmer
+from nltk.corpus import stopwords 
+porter=PorterStemmer()
 
 def read_file(file):
 	# Creates a header (for keys) and a dictionary
@@ -38,7 +42,7 @@ def read_file(file):
 	# Initiates a langid instance that normalizes the probabilities of language identification
 	identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
 
-	for line in lines:
+	for line in lines[0:500]:
 		if identifier.classify(line[6])[0] and identifier.classify(line[6])[1] > 0.5:
 			blogdict["id"].append(line[0])
 			blogdict["gender"].append(line[1])
@@ -47,39 +51,31 @@ def read_file(file):
 			blogdict["sign"].append(line[4])
 			blogdict["date"].append(line[5])
 			blogdict["text"].append(line[6])
-
-
-	print(blogdict["id"][23556],blogdict["gender"][23556],blogdict["age"][23556],blogdict["topic"][23556],
-			blogdict["sign"][23556],
-			blogdict["date"][23556],
-			blogdict["text"][23556])
-	print(len(blogdict["id"][23556]),len(blogdict["gender"][23556]),len(blogdict["age"][23556]),len(blogdict["topic"][23556]),
-			len(blogdict["sign"][23556]),
-			len(blogdict["date"][23556]),
-			len(blogdict["text"][23556]))
 	return blogdict
 
-blogdict = read_file("sample.csv")
-
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.stem import PorterStemmer 
-from nltk.stem import LancasterStemmer
-
-porter=PorterStemmer()
-def stemSentence(sentence):
+def stem_and_remove_stopwords_Sentence(sentence):
+    #Tokenizes the input
     token_words=word_tokenize(sentence)
     token_words
+    stop_words = set(stopwords.words('english')) 
+
     stem_sentence=[]
     for word in token_words:
-        stem_sentence.append(porter.stem(word))
-        stem_sentence.append(" ")
+    	if not word in stop_words:
+    		stem_sentence.append(porter.stem(word))
+    		stem_sentence.append(" ")
     return "".join(stem_sentence)
 
-for i in range(0,len(blogdict["id"])):
-	blogdict["text"][i] = stemSentence(blogdict["text"][i])
+def main(): 
+	blogdict = read_file("sample.csv")
+	for i in range(0,len(blogdict["id"])):
+		blogdict["text"][i] = stem_and_remove_stopwords_Sentence(blogdict["text"][i])
+	print("\n",blogdict["text"][9:11])
 
-print(blogdict["id"][23556],blogdict["gender"][23556],blogdict["age"][23556],blogdict["topic"][23556],
-blogdict["sign"][23556],
-blogdict["date"][23556],
-blogdict["text"][23556])
-print(len(blogdict["text"][23556]))
+if __name__=='__main__':
+	main()
+
+
+
+
+
